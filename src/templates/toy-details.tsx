74e210cf8d80471c6ca05ee0e7aaeb06d395176c
Ajay <ajaysudani07@gmail.com>
2021-09-10 18:50:00 +0530

    Used background image

--- src/templates/toy-details.tsx
+++ src/templates/toy-details.tsx
@@ -1,29 +1,34 @@
 import React from "react";
-import { graphql } from "gatsby";
 import { InView } from "react-intersection-observer";
+import { graphql, useStaticQuery } from "gatsby";
+import BackgroundImage from "gatsby-background-image";
 import "./toy-details.scss";
 
 // markup
 const ToyDetails = ({ data }) => {
+  const imageData = data.backgroundImage.childImageSharp.fluid;
   const { contentfulToy: toy } = data;
+
   return (
-    <div className="toy-details-container">
-      <div className="image-container">
-        <InView>
-          <img src={toy.image.file.url} alt="No image found" />
-        </InView>
+    <BackgroundImage Tag="section" fluid={imageData}>
+      <div className="toy-details-container">
+        <div className="image-container">
+          <InView>
+            <img src={toy.image.file.url} alt="No image found" />
+          </InView>
+        </div>
+        <div className="details">
+          <p className="name">
+            <b>{toy.name}</b>
+          </p>
+          <p className="description">
+            {JSON.parse(toy.description.raw).content[0].content[0].value}
+          </p>
+          <p className="price">Price : {toy.price}</p>
+          <p className="rating">Rating : {toy.rating}</p>
+        </div>
       </div>
-      <div className="details">
-        <p className="name">
-          <b>{toy.name}</b>
-        </p>
-        <p className="description">
-          {JSON.parse(toy.description.raw).content[0].content[0].value}
-        </p>
-        <p className="price">Price : {toy.price}</p>
-        <p className="rating">Rating : {toy.rating}</p>
-      </div>
-    </div>
+    </BackgroundImage>
   );
 };
 
@@ -41,6 +46,13 @@ export const query = graphql`
       }
       price
       rating
+    }
+    backgroundImage: file(relativePath: { eq: "bg.jpg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 1200) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
     }
   }
 `;
